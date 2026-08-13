56df8c2cb89f24b0e3002c5d731a341574e9fe0e
Kaze-ui <i.fischer@alumno.etec.um.edu.ar>
2026-08-13 00:22:56 -0300

    Setup inicial de Canvas, HUD y PausePanel en escena InGame

---
+++ Assets/Scenes/MainMenú.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private string MenúPrincipal = "Game";
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(MenúPrincipal);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Cerrando el juego...");
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
